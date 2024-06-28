2cbb801a7f0a7ccf91fd7a9469148db5cfc1f617
TruyenBD <mrtruyenbd1407@gmail.com>
2024-06-28 23:34:37 +0700

    change composables

--- composables/fetch.ts
+++ composables/fetch.ts
@@ -2,14 +2,14 @@ type Method = "GET" | "POST" | "PUT" | "DELETE";
 type options = {query?:any, body?:any, headers?:any, method?:Method, baseURL?:string}
 export async function fetchAPI(method: Method, url: string, body: any | null) {
   try {
-    const res = await $fetch(url, fetchOptions(method, body || null));
+    const res = await fetch(url, fetchOptions(method, body || null));
     return res;
   } catch (error) {
     // Handle permission error Authorization
 
     // if (error.response._data.error.code == "ERR.TOK0102") {
     //   await refreshToken();
-    //   return $fetch(url, fetchOptions(method, body)).then((res_1) => res_1);
+    //   return fetch(url, fetchOptions(method, body)).then((res_1) => res_1);
     // }
     // else if (error.response._data.error.code == "ERR.TOK0101")  {
     //   console.log("logout o fetch custom")
@@ -36,13 +36,13 @@ export function fetchOptions(method:Method,body:any) {
 
 // export async function fetchAPI(method,url, body) {
 //   try {
-//     const res = await $fetch(url, fetchOptions(method, body));
+//     const res = await fetch(url, fetchOptions(method, body));
 //     return res;
 //   } catch (error) {
 //     console.log("error:", error)
 //     if (error.response._data.error.code == "ERR.TOK0102") {
 //       await refreshToken();
-//       return $fetch(url, fetchOptions(method, body)).then((res_1) => res_1);
+//       return fetch(url, fetchOptions(method, body)).then((res_1) => res_1);
 //     }
 //     else if (error.response._data.error.code == "ERR.TOK0101")  {
 //       console.log("logout o fetch custom")
